656a85ae9805e9b1ee1e045f66f85fc873c6589e
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2022-08-12 18:32:07 -0300

    feat: config base style and fonts

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,22 @@
+export const defaultTheme = {
+  'white-100': '#FFFFFF',
+
+  'gray-100': '#FAFAFA',
+  'gray-200': '#EEF2F6',
+  'gray-300': '#D7D7D7',
+  'gray-400': '#DDE5ED',
+  'gray-500': '#AEBFD0',
+  'gray-600': '#8AA4BD',
+  'gray-700': '#AAAFC5',
+  'gray-800': '#5E6277',
+  'gray-900': '#3F4252',
+
+  'brand-100': '#F04E98',
+  'secondary-100': '#00BFA5',
+  
+  'black': '#1D1F2A',
+  'blue-100': '#344383',
+  'blue-500': '#141B4D',
+  'red-500': '#E40022',
+  
+};
