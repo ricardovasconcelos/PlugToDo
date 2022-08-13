e8bb58e9af98f59d66cb1f730b5042e244f21668
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2022-08-13 17:55:28 -0300

    feat: add Router and create home page

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,15 @@ import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from './styles/themes/default';
 import { GlobalStyle } from './styles/global';
 
+import { Router } from './Router';
+import { BrowserRouter } from 'react-router-dom';
+
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>PlugToDo</h1>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   );
