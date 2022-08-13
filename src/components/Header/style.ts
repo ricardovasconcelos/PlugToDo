15589e700221e1a1cbf1628f3568c9b509adaa45
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2022-08-13 18:06:45 -0300

    feat: create Header component and add it to layout

---
+++ src/components/Header/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 3.75rem;
+
+  img {
+    margin-left: 1rem;
+  }
+`;
--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,3 +1,5 @@
 import styled from 'styled-components';
 
-export const LayoutContainer = styled.div``;
+export const LayoutContainer = styled.div`
+  height: 100vh;
+`;
