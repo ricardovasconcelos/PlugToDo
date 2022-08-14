b192561c853fd07335ec65142790bb6d7cf28a32
Ricardo Vasconcelos <ricardovasconcelos.dev@gmail.com>
2022-08-14 11:12:11 -0300

    feat: add navigate to home button and header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,13 @@
 import { HeaderContainer } from './styles';
 import logo from '../../assets/logo.svg';
+import { NavLink } from 'react-router-dom';
 
 export function Header() {
   return (
     <HeaderContainer>
-      <img src={logo} alt='Plug Logo' />
+      <NavLink to='/'>
+        <img src={logo} alt='Plug Logo' />
+      </NavLink>
     </HeaderContainer>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,18 @@
-import { HomeContainer } from './styles';
-import Illustration from '../../assets/Illustration.svg';
+import { useNavigate } from 'react-router-dom';
 import { Button } from '../../components/Button';
+import Illustration from '../../assets/Illustration.svg';
+
+import { HomeContainer } from './styles';
 
 export function Home() {
+  const navigate = useNavigate();
+
   return (
     <HomeContainer>
       <img src={Illustration} alt='' />
-      <Button width={13.3}>INICIAR</Button>
+      <Button width={13.3} onClick={() => navigate('/task')}>
+        INICIAR
+      </Button>
     </HomeContainer>
   );
 }
